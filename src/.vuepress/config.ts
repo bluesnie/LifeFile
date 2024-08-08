ef265cede70a465dc14b8c05828bb57289a07f4e
bluesnie <lovekkfan@hotmail.com>
2024-08-08 08:18:13 +0800

    update docs

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,8 +6,10 @@ import { getDirname, path } from "@vuepress/utils";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/MyLife/",
-  // base: "/",
+  // base: "/MyLife/",  // 在线github-pages 部署
+  base: "/",           // 本地调试npm run docs:dev 或 本地部署，python3 -m http.server 8080
+
+  dest: 'docs', // 打包输出路径
 
   lang: "zh-CN",
   title: "我的人生",
